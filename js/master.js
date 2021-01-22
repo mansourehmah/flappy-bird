0ea0fb697a85269b0cdde23a58f37246df59d563
mansoureh <mansoureh.1998@gmail.com>
2021-01-22 06:41:29 +0330

    setting box(change bird)

--- js/master.js
+++ js/master.js
@@ -14,14 +14,37 @@ function setting() {
   document.querySelector("body").classList.toggle("goTop");
 }
 
+birds = ["bird.svg", "bird_blue.svg", "bird_red.svg"];
+index=0
+function changeBird(btn) {
+  if (btn == "next") {
+      if(index!=2){
+          index++
+      }
+      else{
+          index=0
+      }
+  }
+  else{
+      if(index==0){
+          index=2
+      }
+      else{
+          index--
+      }
+  }
+  document.querySelector('.bird').src = 'img/' + birds[index]
+  document.querySelector('.changeBird .bird').src = 'img/' + birds[index]
+}
+
 function changeBtn(btn) {
-  if (btn == 'night') {
+  if (btn == "night") {
     document.querySelector(".night span").classList.toggle("changebtn");
     document.querySelector(".night").classList.toggle("activeBtn");
-    document.querySelector(".sky").classList.toggle('changeSky');
+    document.querySelector(".sky").classList.toggle("changeSky");
     document.querySelector(".cloud").classList.toggle("changeOp");
   }
-  if (btn == 'hyper') {
+  if (btn == "hyper") {
     document.querySelector(".hyper span").classList.toggle("changebtn");
     document.querySelector(".hyper").classList.toggle("activeBtn");
   }
