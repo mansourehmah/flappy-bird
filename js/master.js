4a805d3c753fe9fc9ee86f1176d138d74c0f954e
mansoureh <mansoureh.1998@gmail.com>
2021-01-22 05:25:01 +0330

    edit background and pipe

--- js/master.js
+++ js/master.js
@@ -1,6 +1,9 @@
-function playGame(){
-    document.querySelector('.play').classList.add('displayNone')
-    document.querySelector('.ground').classList.add('playAnimation')
-    document.querySelector('.cloud').classList.add('playAnimation')
-    document.querySelector('.pipe').classList.add('pipe_move')
+var pipe = document.querySelector(".pipe");
+var character = document.querySelector(".character");
+function playGame() {
+  document.querySelector(".play").classList.add("displayNone");
+  document.querySelector(".ground").classList.add("playAnimation");
+  document.querySelector(".cloud").classList.add("playAnimation");
+  document.querySelector(".pipe").classList.add("pipe_move");
+  document.querySelector(".hole").classList.add("pipe_move");
 }
