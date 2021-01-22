b731164dc9266c1579eaac368f06a797164ad4d2
mansoureh <mansoureh.1998@gmail.com>
2021-01-22 05:00:51 +0330

    edit background

---
+++ js/master.js
@@ -0,0 +1,6 @@
+function playGame(){
+    document.querySelector('.play').classList.add('displayNone')
+    document.querySelector('.ground').classList.add('playAnimation')
+    document.querySelector('.cloud').classList.add('playAnimation')
+    document.querySelector('.pipe').classList.add('pipe_move')
+}
